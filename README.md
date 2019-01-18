9e2c17f1d80d592af93e5c5d1c03a1ec057c78bd
chencheng <sorrycc@gmail.com>
2019-01-18 18:16:49 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# import-helper
+
+[WIP] Import helper for your code.
+
+[![NPM version](https://img.shields.io/npm/v/import-helper.svg?style=flat)](https://npmjs.org/package/import-helper)
+[![Build Status](https://img.shields.io/travis/sorrycc/import-helper.svg?style=flat)](https://travis-ci.org/sorrycc/import-helper)
+[![NPM downloads](http://img.shields.io/npm/dm/import-helper.svg?style=flat)](https://npmjs.org/package/import-helper)
+
+## Why
+
+解决手写 import 的重复劳动，能交给机器做的绝不自己动手。
+
+## Demo
+
+![](https://img.alicdn.com/tfs/TB1RI92CFzqK1RjSZFoXXbfcXXa-817-623.gif)
+
+## Features
+
+* ✔︎ 根据 identifier 自动补全 import
+* ✘︎ 根据 package.json 中的依赖补全 import
+* ✘︎ 支持 esm 和 cjs（目前仅支持 esm）
+
+## Installation
+
+```bash
+$ yarn global add import-helper
+```
+
+## Usage
+
+## LICENSE
+
+MIT
---
+++ cli.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+const { existsSync, readFileSync, writeFileSync } = require('fs');
+const { join } = require('path');
+const yParser = require('yargs-parser');
+const chalk = require('chalk');
+
+// print version and @local
+const args = yParser(process.argv.slice(2));
+if (args.v || args.version) {
+  console.log(require('./package').version);
+  if (existsSync(join(__dirname, './.local'))) {
+    console.log(chalk.cyan('@local'));
+  }
+  process.exit(0);
+}
+
+if (!args._[0]) {
+  console.error(chalk.red(`> Please specify the file.`));
+  process.exit(1);
+}
+
+const root = args.root || process.cwd();
+const file = join(root, args._[0]);
+const code = require('./lib/addImports')(readFileSync(file, 'utf-8'), {
+  ...require('./lib/module/node'),
+});
+
+if (args.write) {
+  writeFileSync(file, code, 'utf-8');
+} else {
+  console.log(code);
+}
